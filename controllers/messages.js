af8179e5b994af10ced956dcf098d9c4326b5a69
MarieSerroyen <marie.serroyen@gmail.com>
2022-04-23 10:16:14 +0200

    feat: deleting message works;

--- controllers/messages.js
+++ controllers/messages.js
@@ -70,7 +70,26 @@ const update = (req, res) => {
     res.json(response);
 };
 
+const deleteMessage = (req, res) => {
+    const id = req.params.id;
+
+    const response = {
+        status: "success",
+        message : `DELETING a message with id ${id}`,
+        data : {
+            messages : [
+                {
+                    "message" : "The message was removed"
+                }
+            ]
+        }
+    };
+    res.json(response);
+};
+
+//exports
 module.exports.getAll = getAll;
 module.exports.getId = getId;
 module.exports.create = create;
 module.exports.update = update;
+module.exports.deleteMessage = deleteMessage;
